e9b141acfabc15780f6ea368b7f974512c4e8c6a
minhthuan1809 <thuan1809minh@gmail.com>
2024-06-06 16:39:57 +0700

    bai 22

--- day-22/js/bai2.js
+++ day-22/js/bai2.js
@@ -1,13 +1,27 @@
-var arr = [3, 4, 5, 6, 7, 2, 3, 45, 5, 67, 8, 9, 5, 4];
-var number = [];
-for (var index in arr) {
-  if (arr[index] < 2) {
-    continue;
-  } else if (arr[index] % 2 === 0 && arr[index] % 3 === 0) {
-    continue;
-  } else {
-    number[number.length] = arr[index];
+function isNumber(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 1) {
+      continue;
+    }
+    if (arr[i] === 2 || arr[i] === 3) {
+      Sum[Sum.length] = arr[i];
+    }
+    if (arr[i] % 2 === 0 || arr[i] % 3 === 0) {
+      continue;
+    } else {
+      Sum[Sum.length] = arr[i];
+    }
   }
 }
+function isSum() {
+  var Total = 0;
+  isNumber(_arr);
+  for (var index in Sum) {
+    Total += Sum[index];
+  }
+  return Total;
+}
+var _arr = [3, 4, 5, 6, 7, 2, 3, 45, 5, 67, 8, 9, 5, 4];
+var Sum = [];
 
-console.log(number);
+document.write(`Tổng các số nguyên tố trong mảng là :  ${isSum()}`);
