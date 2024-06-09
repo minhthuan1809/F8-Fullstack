57f6f0932c616a5c7fa38a7c77bdc96dc6abd608
minhthuan1809 <Thuan1809minh@gmail.com>
2024-06-10 02:34:58 +0700

    buoi 23

--- day-23/js/ex02.js
+++ day-23/js/ex02.js
@@ -1,4 +1,3 @@
-var arr = [0, 1, [2, 3], [4, 5, [6, 7]], [8, [9, 10, [11, 12]]]];
 function _array(_arr) {
   var result = [];
   _arr.forEach(function (value) {
@@ -10,5 +9,9 @@ function _array(_arr) {
   });
   return result;
 }
-
-console.log(_array(arr));
+var arr = [0, 1, [2, 3], [4, 5, [6, 7]], [8, [9, 10, [11, 12]]]];
+document.write(
+  ` cho mảng sau : [0, 1, [2, 3], [4, 5, [6, 7]], [8, [9, 10, [11, 12]]]] <br> viết lại mảng  ${_array(
+    arr
+  )}`
+);
