d6e5602a3b3d569016658a708d7e562a348a1110
minhthuan1809 <Thuan1809minh@gmail.com>
2024-08-06 04:10:05 +0700

    f8

--- day-37/script.js
+++ day-37/script.js
@@ -6,6 +6,7 @@ var data = {
   youtube: "https://www.youtube.com/",
   facebook: "https://www.facebook.com/",
   f8: "https://fullstack.edu.vn/",
+  google: "https://www.google.com.vn/?hl=vi",
   "google drive":
     "https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwjR77f5zt6HAxXmOHsHHcY-K1gYABAAGgJ0bQ&co=1&ase=2&gclid=Cj0KCQjw8MG1BhCoARIsAHxSiQkwcLw5I35wCvZFcvK6o_PalITQhMAmztur3FzY2Dh8i0h7MNy-7XIaAoj3EALw_wcB&ohost=www.google.com&cid=CAESVuD2NwJDWBIDX8AMuNLcduToDx5bC2Hf4K90lQAn9z7RAF6KMuMIeNVaMaZ3o7XBDPgO2CL72xRMJlatMTmMJqmeB8zqXMZ4kNrIHBMSLLtZNGo4uIfZ&sig=AOD64_3cNrXP17JmQGz3wStYtRfrIX9bBg&q&nis=4&adurl&ved=2ahUKEwi0mLP5zt6HAxWZkVYBHVjZF2QQ0Qx6BAgJEAE",
 };
@@ -42,16 +43,53 @@ if ("webkitSpeechRecognition" in window) {
         messEl.classList.add("active");
         messEl.innerText = `${key} đang được mở`;
         found = true;
-      } else if (value === "") {
+      } else if (
+        (value.toLowerCase().trim().includes("đi") &&
+          value.toLowerCase().trim().includes("đường")) ||
+        (value.toLowerCase().trim().includes("đi") &&
+          value.toLowerCase().trim().includes("đến")) ||
+        (value.toLowerCase().trim().includes("chỉ") &&
+          value.toLowerCase().trim().includes("đường")) ||
+        value.toLowerCase().trim().includes("đi") ||
+        value.toLowerCase().trim().includes("đến")
+      ) {
+        const map = value
+          .replace("tới", "")
+          .replace("chỉ", "")
+          .replace("đường", "")
+          .replace("đến", "");
+
         messEl.classList.add("active");
-        messEl.innerText = `${key} Bạn không nói gì cả `;
+        messEl.innerText = `"đang mở map ${value}" `;
+        setTimeout(() => {
+          window.open(`https://www.google.com/maps/search/${value}`, "_blank");
+        }, 1000);
+        found = true;
+      } else if (
+        value.toLowerCase().trim().includes("bài hát") ||
+        value.toLowerCase().trim().includes("mở bài") ||
+        value.toLowerCase().trim().includes("hát") ||
+        value.toLowerCase().trim().includes("nhạc")
+      ) {
+        const music = value.replace("bài hát", "").replace("mở bài", "");
+        messEl.innerText = `"đang mở bài ${value}" `;
+        messEl.classList.add("active");
+        setTimeout(() => {
+          window.open(
+            `https://zingmp3.vn/tim-kiem/bai-hat?q=${music}`,
+            "_blank"
+          );
+        }, 1000);
+        found = true;
       }
     });
 
     if (!found) {
       messEl.classList.remove("active");
       messEl.innerText = `"${value}" không có trong từ điển của tôi, nhưng tôi có thể gọi ý cho bạn`;
-      window.open(`https://www.google.com/search?q=${value}`, "_blank");
+      setTimeout(() => {
+        window.open(`https://www.google.com/search?q=${value}`, "_blank");
+      }, 1000);
     }
   }
 
