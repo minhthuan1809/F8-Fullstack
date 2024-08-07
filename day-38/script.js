a2d2f118b870af4b04dda47336e7d1267b360544
minhthuan1809 <thuan1809minh@gmail.com>
2024-08-08 06:17:22 +0700

    f8

--- day-38/script.js
+++ day-38/script.js
@@ -1,7 +1,6 @@
 const btnCompleted = document.querySelector(".btn--conpleted");
 const iconEl = document.querySelector(".icon");
 btnCompleted.addEventListener("click", () => {
-  btnCompleted.classList.add("active--btn");
-  iconEl.classList.add("active--icon");
-  console.log("thuaanj");
+  btnCompleted.classList.toggle("active--btn");
+  iconEl.classList.toggle("active--icon");
 });
