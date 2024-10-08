194f768e456dc0f0bfd7bac44bc063bef0020a59
minhthuan1809 <thuan1809minh@gmail.com>
2024-10-08 08:16:50 +0700

    f8

--- day-48/src/App.jsx
+++ day-48/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import NumberGuessingGame from "./assets/components/NumberGuessingGame";
+import NumberGuessingGame from "../assets/components/NumberGuessingGame";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 export default function App() {
+  console.log("Minh Thuận");
+
   return (
     <div>
       <NumberGuessingGame />
