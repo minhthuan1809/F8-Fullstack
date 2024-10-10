613c3eb10d4d5c1a030361018121a7d70cd196a8
minhthuan1809 <thuan1809minh@gmail.com>
2024-10-11 05:45:59 +0700

    f8

--- day-49/src/redux/action/cartAction.js
+++
@@ -1,8 +0,0 @@
-export const addCart = (value) => ({ type: "cart/add", payload: value });
-export const deleteCart = (value) => ({
-  type: "cart/delete",
-  payload: { _id: value.id },
-});
-export const PayCart = () => ({
-  type: "cart/pay",
-});
